c24c2f16a6d8afa5b520fdc54fab58b0eaba0cfa
[cepcema] <[qvor-dimitrov@hotmail.com]>
2022-08-15 14:33:31 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// const getTicekr = () => {
+//     fetch(`http://localhost:8000/tickers`)
+//         .then((response) => response.json())
+//         .then((json) => {
+//             // console.log(json);
+//         })
+//         .catch((err) => console.log(err));
+// };
+// getTicekr();
